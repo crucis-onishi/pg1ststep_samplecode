b0af6df3bf16361a38611d61ad1453167d5436d1
crucis-onishi <crucis@crucis.xsrv.jp>
2020-02-15 07:16:04 +0900

    まだ書いてないところのコード削除

--- battle.py
+++
@@ -1,4 +0,0 @@
-def attack(ad = 100, skill = 2):
-    damage = ad * skill
-    print("勇者は" + str(damage) + "のダメージを与えた")
-print("ここにprint関数などがあると…")
--- mission8.py
+++
@@ -1,6 +0,0 @@
-import csv
-yamato = {"キャラクター": "大和", "装備1": "46cm三連装砲", "装備2": "15.5cm三連装副砲", "装備3": "零式水上観測機"}
-
-with open("/Users/User/Desktop/python_lessons/mission8.csv",mode="a",newline="",encoding="utf-8") as f:
-    writer = csv.DictWriter(f, ["キャラクター", "装備1", "装備2", "装備3"])
-    writer.writerow(yamato)
