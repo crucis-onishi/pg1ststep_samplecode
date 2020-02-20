a6beb793edf3e68ad2c3ed7cf53b120e76970d31
crucis-onishi <crucis@crucis.xsrv.jp>
2020-02-20 17:22:14 +0900

    mission4,5改良

--- mission4.py
+++ mission4.py
@@ -3,7 +3,7 @@ player_hp = 100 # 勇者の初期HPを設定
 maou_hp = 100 # 魔王の初期HPを設定
 print("魔王が現れた\n")
 
-while player_hp > 0 and maou_hp > 0: # 戦闘ループ開始
+while True: # 戦闘ループ開始
 	print("勇者の残りHP" + str(player_hp) + "\t魔王の残りHP：" + str(maou_hp)) # 現在のHPを表示
 	player_attack = randint(10, 20) # 勇者の今回の攻撃
 	print("勇者の攻撃")
@@ -11,6 +11,7 @@ while player_hp > 0 and maou_hp > 0: # 戦闘ループ開始
 	maou_hp -= player_attack # 魔王のHPを減算
 	if maou_hp <= 0: # 魔王のHPが0以下なら
 		print("魔王を倒した！\n")
+        break
 	else: # 魔王のHPが0よりも大きい場合
 		maou_attack = randint(10, 20) # 魔王の今回の攻撃
 		print("魔王の攻撃")
@@ -18,3 +19,4 @@ while player_hp > 0 and maou_hp > 0: # 戦闘ループ開始
 		player_hp -= maou_attack # 勇者のHPを減算
 		if player_hp <= 0: # 勇者のHPが0以下なら
 			print("あなたはしにました\n")
+            break
