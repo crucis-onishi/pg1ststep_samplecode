934dbbfb8ece5f5bb7bca25297cb88b652c36f34
crucis-onishi <crucis@crucis.xsrv.jp>
2020-02-20 15:36:31 +0900

    mission3改良

--- mission3.py
+++ mission3.py
@@ -1,18 +1,16 @@
-flag = False
-while flag == False:
+while True:
     print("問題：この本のタイトルはなんでしょう？")
     print("1.Pythonの教科書")
     print("2.ゼロから学ぶPython")
     print("3.Python3で学ぶプログラミング入門")
     ans = int(input("答えは？"))
     if ans == 3:
-        print("正解！\n")
-        flag = True
+        print("正解！")
+        break
     else:
         print("残念！\n")
 
-flag = False
-while flag == False:
+while True:
     print("問題：次のうちPythonに当てはまるのは？")
     print("1.静的型付け言語")
     print("2.動的型付け言語")
@@ -20,12 +18,11 @@ while flag == False:
     ans = int(input("答えは？"))
     if ans == 2:
         print("正解！\n")
-        flag = True
+        break
     else:
         print("残念！\n")
 
-flag = False
-while flag == False:
+while True:
     print("問題：次のうちPythonに当てはまるのは？")
     print("1.コードがシンプルで扱いやすい汎用言語")
     print("2.国産のプログラミング言語")
@@ -33,6 +30,6 @@ while flag == False:
     ans = int(input("答えは？"))
     if ans == 1:
         print("正解！\n")
-        flag = True
+        break
     else:
         print("残念！\n")
